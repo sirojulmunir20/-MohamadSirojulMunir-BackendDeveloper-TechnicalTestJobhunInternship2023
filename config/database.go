9cf10d7361ada3b398cae988e5283f4545d25582
Mohamad Sirojul Munir <sirojulmunir616@gmail.com>
2023-04-21 10:32:21 +0700

    create connetion database

--- config/database.go
+++ config/database.go
@@ -1,1 +1,37 @@
 package config
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+  _ "github.com/go-sql-driver/mysql"
+)
+
+const (
+	username = "root"
+	password = ""
+	host     = "localhost"
+	port     = "3306"
+	dbname   = "jobhun_test"
+)
+
+func NewDB() (*sql.DB, error) {
+	// create the data source name
+	dsn:= fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, host, port, dbname)
+
+	// open  database connection
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("failed to open connection database: %v", err)
+		return nil,err
+	}
+
+	// check if the connection is valid
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	fmt.Println("Connected to database!")
+	return db, nil
+}
--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
+import (
+	"jobhun-api/config"
+	"log"
+)
 
 func main() {
-	
+	db, err := config.NewDB()
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	defer db.Close()
 }
