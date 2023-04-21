a9b1093563d0a23bca2d89f32c0fe30b7b798578
Mohamad Sirojul Munir <sirojulmunir616@gmail.com>
2023-04-21 11:31:50 +0700

    first server

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"jobhun-api/config"
+	"jobhun-api/helper"
 	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
+	fmt.Println("Start server")
+	
 	db, err := config.NewDB()
 	if err != nil {
 		log.Fatalf("failed to connect to database: %v", err)
 	}
-
+	
 	defer db.Close()
+	
+	routes := httprouter.New()
+
+	routes.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		fmt.Fprint(w, "Welcome Home")
+	})
+
+	server := http.Server{Addr: "localhost:8888", Handler: routes}
+
+	err = server.ListenAndServe()
+	helper.PanicIfError(err)
+	
 }
--- model/student.go
+++ model/student.go
@@ -1,1 +1,29 @@
 package model
+
+import "time"
+
+type Mahasiswa struct {
+	ID         int       `json:"id"`
+	Nama       string    `json:"nama"`
+	Usia       int       `json:"usia"`
+	Gender     int       `json:"gender"`
+	TanggalReg time.Time `json:"tanggal_reg"`
+	Jurusan    Jurusan   `json:"jurusan"`
+	Hobi       []Hobi    `json:"hobi"`
+}
+
+type Jurusan struct {
+	ID   int    `json:"id"`
+	Nama string `json:"nama"`
+}
+
+type Hobi struct {
+	ID   int    `json:"id"`
+	Nama string `json:"nama"`
+}
+
+type MahasiswaHobi struct {
+	ID          int `json:"id"`
+	IDMahasiswa int `json:"id_mahasiswa"`
+	IDHobi      int `json:"id_hobi"`
+}
